b923877937c7799d7e67788c0235e5814fd886aa
dinocular <hhben10@gmail.com>
2014-11-14 20:24:37 -0500

    fresh

--- platforms/android/CordovaLib/ant-build/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-C:\Users\Ricardo\Documents\GitHub\Transl8Me\platforms\android\CordovaLib\ant-build\AndroidManifest.xml : \
-C:\Users\Ricardo\Documents\GitHub\Transl8Me\platforms\android\CordovaLib\AndroidManifest.xml \
--- platforms/android/ant-build/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-C:\Users\Ricardo\Documents\GitHub\Transl8Me\platforms\android\ant-build\AndroidManifest.xml : \
-C:\Users\Ricardo\Documents\GitHub\Transl8Me\platforms\android\AndroidManifest.xml \
